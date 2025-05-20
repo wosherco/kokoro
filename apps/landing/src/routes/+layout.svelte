c5ecec8960df042a62fb543a363e9dcbfd0e40ff
polvallverdu <pvallverdualcaide@gmail.com>
2025-05-20 23:03:24 +0200

    feat: migrated to new paraglide i18n

--- apps/landing/src/routes/+layout.svelte
+++ apps/landing/src/routes/+layout.svelte
@@ -1,49 +1,52 @@
 <script lang="ts">
-import { page } from "$app/stores";
-import { Toaster } from "$lib/components/ui/sonner/index.js";
-import { i18n } from "$lib/i18n";
-import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+  import { page } from "$app/state";
+  import { Toaster } from "$lib/components/ui/sonner/index.js";
+  import { locales, localizeHref } from "$lib/paraglide/runtime";
 
-import Footer from "../components/Footer.svelte";
-import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
+  import Header from "../components/Header.svelte";
 
-import "../app.css";
+  import "../app.css";
 
-import { browser } from "$app/environment";
-import { afterNavigate, beforeNavigate, onNavigate } from "$app/navigation";
-import posthog from "posthog-js";
+  import { browser } from "$app/environment";
+  import { afterNavigate, beforeNavigate, onNavigate } from "$app/navigation";
+  import posthog from "posthog-js";
 
-const { children } = $props();
+  const { children } = $props();
 
-beforeNavigate(({ to, from }) => {
-  if (browser) {
-    posthog.capture("$pageleave");
-  }
-});
+  beforeNavigate(({ to, from }) => {
+    if (browser) {
+      posthog.capture("$pageleave");
+    }
+  });
 
-afterNavigate(({ to }) => {
-  if (browser) {
-    posthog.capture("$pageview");
-  }
-});
+  afterNavigate(({ to }) => {
+    if (browser) {
+      posthog.capture("$pageview");
+    }
+  });
 
-onNavigate((navigation) => {
-  if (!document.startViewTransition) return;
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
 
-  return new Promise((resolve) => {
-    document.startViewTransition(async () => {
-      resolve();
-      await navigation.complete;
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
     });
   });
-});
 </script>
 
-<ParaglideJS {i18n}>
-  <Toaster />
-  <Header pathname={$page.url.pathname} />
-  <main class="min-h-[100dvh]">
-    {@render children()}
-  </main>
-  <Footer />
-</ParaglideJS>
+<Toaster />
+<Header pathname={page.url.pathname} />
+<main class="min-h-[100dvh]">
+  {@render children()}
+</main>
+<Footer />
+
+<div style="display:none">
+  {#each locales as locale}
+    <a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+  {/each}
+</div>
