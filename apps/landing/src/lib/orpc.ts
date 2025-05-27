aab14aa91d516d99e57888c76a87407a9bddbd46
polvallverdu <pvallverdualcaide@gmail.com>
2025-05-27 17:01:08 +0200

    fix: using constant on landing

--- apps/landing/src/lib/orpc.ts
+++ apps/landing/src/lib/orpc.ts
@@ -1,11 +1,11 @@
-import { env } from "$env/dynamic/public";
+import { PUBLIC_API_URL } from "$env/static/public";
 import type { orpcContract } from "@kokoro/validators/contracts";
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import type { ContractRouterClient } from "@orpc/contract";
 
 const link = new RPCLink({
-  url: `${env.PUBLIC_API_URL}/rpc`,
+  url: `${PUBLIC_API_URL}/rpc`,
   fetch: async (url, options) =>
     fetch(url, { ...options, credentials: "include" }),
   headers: () => {
